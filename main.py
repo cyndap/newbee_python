dbb296afffd87a2aa4dc6f6c319a1204f5b60991
Cynda P <126617031+cyndap@users.noreply.github.com>
2023-04-16 13:08:52 -0500

    Add files via upload

---
+++ main.py
@@ -0,0 +1,14 @@
+# num = int(input("Put a number here and watch the magic: "))
+# if num % 2 == 0 and num % 4 !=0:
+#     print("Your number is even.")
+# elif num % 4 == 0:
+#     print ("Your number is divisible by uhhhh 4 I think.")
+# else: 
+#     print ("Odd number like yo face.")
+
+num = int(input("Enter a number here: "))
+check = int(input("And how about another here: "))
+if check%num == 0:
+  print ("Your number is evenly divisible.")
+else: 
+    print ("Your pair of numbers are not evenly divisible.")
